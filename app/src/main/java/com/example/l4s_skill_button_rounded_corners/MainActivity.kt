194c57e600c71f9cf173216c656156682baf1e38
numabesachi <numabesachi@gmail.com>
2020-09-15 14:06:25 +0900

    finish

---
+++ app/src/main/java/com/example/l4s_skill_button_rounded_corners/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.l4s_skill_button_rounded_corners
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
